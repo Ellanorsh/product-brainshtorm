8f5f456a48b0e465d3f95b984a0cafd5cf29ef08
Ivan Karmanov <ellanorsh@Ivans-MacBook-Pro.local>
2025-05-16 11:41:51 -0600

    Fix: переместил app = Flask(...) выше @app.route

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask import Flask, request, jsonify, render_template
 import os
+
+app = Flask(__name__)  # <-- сначала создаём объект Flask
+
 print("TEMPLATE_FOLDER:", os.path.abspath("templates"))
 
 @app.route("/")
 def home():
-    print("⚡️ Рендер шаблона запущен")
-    return render_template("index.html")
+    return render_template("index.html")  # <-- потом используем app
