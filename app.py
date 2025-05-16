e9f3cf90ccea674ff5e321893d7552e940515f0f
Ivan Karmanov <ellanorsh@Ivans-MacBook-Pro.local>
2025-05-16 12:08:14 -0600

    Align with working project setup

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    html = render_template("index.html")
-    response = make_response(html)
-    response.headers["Content-Type"] = "text/html"
-    return response
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
