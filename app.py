bbf59b6093e66327f5fc0b56ce246d6a7b5dee83
Ivan Karmanov <ellanorsh@Ivans-MacBook-Pro.local>
2025-05-16 11:57:50 -0600

    Minimal working Flask app to test render_template

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, request, jsonify, render_template, make_response
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-print("TEMPLATE_FOLDER:", os.path.abspath("templates"))
-
 @app.route("/")
 def home():
-    html = render_template("index.html")
-    response = make_response(html)
-    response.headers['Content-Type'] = 'text/html'
-    return response
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
