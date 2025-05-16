641471219c06b28255230eef1b1a938df12448fe
Ivan Karmanov <ellanorsh@Ivans-MacBook-Pro.local>
2025-05-16 11:59:44 -0600

    Force Content-Type to text/html

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    html = render_template("index.html")
+    response = make_response(html)
+    response.headers["Content-Type"] = "text/html"
+    return response
 
 if __name__ == "__main__":
     app.run()
