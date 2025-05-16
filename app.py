ec1d73517f4633b6284312f16ac3688f380bf34c
Ivan Karmanov <ellanorsh@Ivans-MacBook-Pro.local>
2025-05-16 11:48:25 -0600

    Fix: форсируем Content-Type как text/html

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ print("TEMPLATE_FOLDER:", os.path.abspath("templates"))
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    html = render_template("index.html")
+    response = make_response(html)
+    response.headers['Content-Type'] = 'text/html'
+    return response
 
 # запуск локально (Heroku игнорирует этот блок, но он нужен для отладки)
 if __name__ == "__main__":
